61a4b2ddf81c070480411916d1de7fa54ae389ff
rysavyvladan <rysavy.vladan@gmail.com>
2018-04-08 16:38:22 +0200

    init of repo

---
+++ gorm/README.md
@@ -0,0 +1,2 @@
+callbacks:
+Provide save id as string  and used created, updated and deleted as int64 unix time.
---
+++ gorm/callbacks.go
@@ -0,0 +1,69 @@
+package gorm
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/satori/go.uuid"
+	"log"
+	"time"
+)
+
+func RegisterCallbacks(db *gorm.DB) {
+	db.Callback().Create().Before("gorm:create").Register("my_plugin:before_create", beforeCreate)
+	db.Callback().Update().Before("gorm:update").Register("my_plugin:before_update", beforeUpdate)
+	db.Callback().Delete().Before("gorm:delete").Register("my_plugin:before_delete", beforeDelete)
+	db.Callback().Query().Before("gorm:query").Register("my_plugin:before_query", beforeQuery)
+}
+
+func beforeQuery(scope *gorm.Scope) {
+	scope.Search.Where("deleted = 0")
+}
+
+func beforeCreate(scope *gorm.Scope) {
+	id := uuid.NewV4().String()
+
+	field, ok := scope.FieldByName("id")
+	if ok == true && field.IsBlank {
+		err := scope.SetColumn("id", id)
+		if err != nil {
+			log.Fatal("err callback id ", err)
+		}
+	}
+
+	timeNow := time.Now().Unix()
+
+	field, ok = scope.FieldByName("created")
+	if ok == true && field.IsBlank {
+		err := scope.SetColumn("created", timeNow)
+		if err != nil {
+			log.Fatal("err callback created ", err)
+		}
+	}
+
+	field, ok = scope.FieldByName("updated")
+	if ok == true && field.IsBlank {
+		err := scope.SetColumn("updated", timeNow)
+		if err != nil {
+			log.Fatal("err callback updated ", err)
+		}
+	}
+}
+
+func beforeUpdate(scope *gorm.Scope) {
+	field, ok := scope.FieldByName("updated")
+	if ok == true && field.IsBlank {
+		err := scope.SetColumn("updated", time.Now().Unix())
+		if err != nil {
+			log.Fatal("err callback updated ", err)
+		}
+	}
+}
+
+func beforeDelete(scope *gorm.Scope) {
+	field, ok := scope.FieldByName("deleted")
+	if ok == true && field.IsBlank {
+		err := scope.SetColumn("deleted", time.Now().Unix())
+		if err != nil {
+			log.Fatal("err callback deleted ", err)
+		}
+	}
+}
