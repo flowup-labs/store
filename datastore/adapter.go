d61a3915962dd5ed6e3c48f080e2ac161ee33198
Peter Malina <peter.malina@flowup.cz>
2018-04-08 18:27:18 +0200

    feat(query): added generalized query and datastore implementation

--- datastore/adapter.go
+++ datastore/adapter.go
@@ -1,1 +1,45 @@
-package gorm
+package datastore
+
+import (
+	ds "cloud.google.com/go/datastore"
+	"github.com/flowup-labs/store"
+)
+
+type Query struct {
+	ds.Query
+}
+
+func (q *Query) Where(field string, op string, val interface{}) store.Query {
+	q.Query.Filter(field+" "+op, val)
+
+	return q
+}
+
+func (q *Query) Order(field string, flow store.OrderFlow) store.Query {
+	switch flow {
+	case store.OrderflowDescending:
+		q.Query.Order(field)
+	case store.OrderFlowAscending:
+		q.Query.Order("-" + field)
+	}
+
+	return q
+}
+
+func (q *Query) Project(fieldNames ...string) store.Query {
+	q.Query.Project(fieldNames...)
+
+	return q
+}
+
+func (q *Query) Offset(offset int) store.Query {
+	q.Query.Offset(offset)
+
+	return q
+}
+
+func (q *Query) Limit(limit int) store.Query {
+	q.Query.Limit(limit)
+
+	return q
+}
--- query.go
+++ query.go
@@ -1,1 +1,20 @@
 package store
+
+type OrderFlow int
+
+// OrderFlow constants
+const (
+	OrderFlowAscending  OrderFlow = 1
+	OrderflowDescending           = 2
+)
+
+type Query interface {
+	Where(field string, op string, val interface{}) Query
+	Order(field string, flow OrderFlow) Query
+	Project(fieldNames ...string) Query
+	Offset(offset int) Query
+	Limit(limit int) Query
+}
+
+type Transaction interface {
+}
